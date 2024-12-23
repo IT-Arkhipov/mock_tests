684e00a2bc8577591d367434c1e0e21ccec724a7
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2024-12-23 21:50:41 +0300

    add: get and post methods; random useragent

---
+++ utils/api_requests.py
@@ -0,0 +1,24 @@
+import requests
+from requests import Response
+from urllib.parse import urljoin
+from fake_useragent import UserAgent
+
+from utils.init import settings
+
+
+ua = UserAgent()
+headers = {'User-Agent': ua.random}
+
+
+def get(endpoint: str, _headers=None, params=None) -> Response:
+    headers.update(_headers)
+    url = urljoin(settings.base_url, endpoint)
+    response = requests.get(url=url, headers=headers, params=params)
+    return response
+
+
+def post(endpoint: str, _headers=None, params=None, body=None) -> Response:
+    headers.update(_headers)
+    url = urljoin(settings.base_url, endpoint)
+    response = requests.post(url=url, headers=headers, params=params, json=body)
+    return response
