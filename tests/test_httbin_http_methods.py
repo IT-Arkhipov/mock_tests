d35ceb559d8f69bc52ca0b110a281997736cfc41
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2024-12-23 22:04:50 +0300

    edit: class based endpoint

--- tests/test_httbin_http_methods.py
+++ tests/test_httbin_http_methods.py
@@ -1,4 +1,4 @@
-import unittest.mock as mock
+from unittest.mock import patch
 import unittest
 
 from resources.endpoints import endpoint
@@ -12,19 +12,21 @@ class TestGetMethods(unittest.TestCase):
         # ARRANGE
         expected_response_code = 200
         # ACT
-        response = api_requests.get(endpoint.get)
+        response = api_requests.get(self.endpoint)
         # ASSERT
-        self.assertEqual(response.status_code, expected_response_code,
-                         msg=f'Wrong response code: {response.status_code}')
+        self.assertEqual(
+            response.status_code, expected_response_code, msg=f'Wrong response code: {response.status_code}'
+        )
 
     class TestPostMethods(unittest.TestCase):
         endpoint = endpoint.post
 
-    def test_post_method_response_code(self):
-        # ARRANGE
-        expected_response_code = 200
-        # ACT
-        response = api_requests.post(endpoint.post)
-        # ASSERT
-        self.assertEqual(response.status_code, expected_response_code,
-                         msg=f'Wrong response code: {response.status_code}')
+        def test_post_method_response_code(self):
+            # ARRANGE
+            expected_response_code = 200
+            # ACT
+            response = api_requests.post(self.endpoint)
+            # ASSERT
+            self.assertEqual(
+                response.status_code, expected_response_code, msg=f'Wrong response code: {response.status_code}'
+            )
