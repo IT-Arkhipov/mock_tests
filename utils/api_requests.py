5f0bd9b0ad2a1d41baacc8d9f8824f8424cc9e2e
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2024-12-23 22:59:33 +0300

    edit: update headers only if _headers is not None

--- utils/api_requests.py
+++ utils/api_requests.py
@@ -5,20 +5,21 @@ from fake_useragent import UserAgent
 
 from utils.init import settings
 
-
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
 
 def get(endpoint: str, _headers=None, params=None) -> Response:
-    headers.update(_headers)
+    if _headers is not None:
+        headers.update(_headers)
     url = urljoin(settings.base_url, endpoint)
     response = requests.get(url=url, headers=headers, params=params)
     return response
 
 
 def post(endpoint: str, _headers=None, params=None, body=None) -> Response:
-    headers.update(_headers)
+    if _headers is not None:
+        headers.update(_headers)
     url = urljoin(settings.base_url, endpoint)
     response = requests.post(url=url, headers=headers, params=params, json=body)
     return response
