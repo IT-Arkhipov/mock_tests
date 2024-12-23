dbfce8cb6607d61a6df787ed759e6eb948f8055b
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2024-12-23 23:00:04 +0300

    add: mocked test for requests.get

--- tests/test_httbin_http_methods.py
+++ tests/test_httbin_http_methods.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import unittest
 
 from resources.endpoints import endpoint
@@ -13,6 +13,23 @@ class TestGetMethods(unittest.TestCase):
         expected_response_code = 200
         # ACT
         response = api_requests.get(self.endpoint)
+        # ASSERT
+        self.assertEqual(
+            response.status_code, expected_response_code, msg=f'Wrong response code: {response.status_code}'
+        )
+
+    # mocked response test
+    @patch('utils.api_requests.requests')
+    def test_mocked_get_method_response_code(self, requests_mock):
+        # ARRANGE
+        response_mock = MagicMock()
+        response_mock.status_code = 200
+        requests_mock.get.return_value = response_mock
+
+        _endpoint = ''
+        expected_response_code = 200
+        # ACT
+        response = api_requests.get(_endpoint)
         # ASSERT
         self.assertEqual(
             response.status_code, expected_response_code, msg=f'Wrong response code: {response.status_code}'
